ff1af5c9da2d44738b6f08b15ae8b9fa87f6ecd9
Charlene Chetcuti <cchetcu3@MGC000003514.warley.ford.com>
2018-04-10 17:40:57 +0100

    added default loading of students

--- directory.rb
+++ directory.rb
@@ -1,6 +1,6 @@
+@students = []
+
 def interactive_menu
-  @students = []
-  student_count = 0
   loop do
     print_menu
     process(gets.chomp)
@@ -50,14 +50,65 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename)
+  file = File.open(filename, "r")
   file.readlines.each do |line|
   name, cohort = line.chomp.split(',')
-    @students << {name: name, cohort: cohort.to_sym}
+    add_student_to_array(name)
   end
   file.close
 end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print_students_list
+  @students.each_with_index do |student, index|
+    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
+  puts ""
+end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # get the first name
+  name = STDIN.gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    add_student_to_array(name)
+    puts "Now we have #{@students.count} students"
+    # get another name from the user
+    name = STDIN.gets.chomp
+  end
+end
+
+def add_student_to_array(name)
+  @students << {name: name, cohort: :november}
+end
+
+def try_load_students
+  filename = "students.csv"# first argument from the command line
+  return if filename.nil? # get out of the method if it isn't given
+  if File.exists?(filename) # if it exists
+    load_students(filename)
+     puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit # quit the program
+  end
+end
+
+try_load_students
+interactive_menu
+
 # students = [
 #   {name: "Dr. Hannibal Lecter", cohort: :february, height: '5\'11', hobby: "eating"},
 #   {name: "Darth Vader", cohort: :november, height: '5\'7', hobby: "pilates"},
@@ -72,106 +123,88 @@ end
 #   {name: "Norman Bates", cohort: :november, height: '6\'3', hobby: "travelling"}
 # ]
 
-def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
-end
+# def print_with_hobbies_height(students)
+#   students.each_with_index do |student, index|
+#     puts "#{(index+1).to_s.concat(".").ljust(3)} #{student[:name].ljust(30)} #{student[:height].ljust(5)} #{student[:hobby].ljust(10)} (#{student[:cohort]} cohort)"
+#   end
+# end
+#
+# def print_using_while(students)
+#   count = 1
+#   while count < students.length do
+#     puts "#{count}. #{students[count][:name]} (#{students[count][:cohort]} cohort)"
+#     count += 1
+#   end
+# end
+#
+# def print_by_first_letter(students, letter)
+#   puts "Students starting with letter #{letter}: "
+#   students.each do |student|
+#     if student[:name].split("").first == letter
+#       puts "#{student[:name]} (#{student[:cohort]} cohort)"
+#     end
+#   end
+# end
 
-def print_students_list
-  @students.each_with_index do |student, index|
-    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
-  end
-end
+# def print_by_name_length(students, length)
+#   puts "Students with name length shorter than #{length}: "
+#   students.each do |student|
+#     if student[:name].split("").count < length
+#       puts "#{student[:name]} (#{student[:cohort]} cohort)"
+#     end
+#   end
+# end
 
-def print_with_hobbies_height(students)
-  students.each_with_index do |student, index|
-    puts "#{(index+1).to_s.concat(".").ljust(3)} #{student[:name].ljust(30)} #{student[:height].ljust(5)} #{student[:hobby].ljust(10)} (#{student[:cohort]} cohort)"
-  end
-end
+# def input_students_with_more_info
+#   puts "Please enter the names of the students"
+#   puts "To finish, just hit return twice"
+#   # get the first name
+#   name = gets.chomp
+#   # while the name is not empty, repeat this code
+#   while !name.empty? do
+#     # add the student hash to the array
+#     @students << {name: name, cohort: get_cohort, height: get_height, hobby: get_hobby}
+#     if @students.count <= 1
+#       puts "Now we have #{@students.count} student"
+#     else
+#       puts "Now we have #{@students.count} students"
+#     end
+#     # get another name from the user
+#     name = gets.chomp
+#   end
+#   # return the array of students
+#   @students
+# end
 
-def print_using_while(students)
-  count = 1
-  while count < students.length do
-    puts "#{count}. #{students[count][:name]} (#{students[count][:cohort]} cohort)"
-    count += 1
-  end
-end
-
-def print_by_first_letter(students, letter)
-  puts "Students starting with letter #{letter}: "
-  students.each do |student|
-    if student[:name].split("").first == letter
-      puts "#{student[:name]} (#{student[:cohort]} cohort)"
-    end
-  end
-end
-
-def print_by_name_length(students, length)
-  puts "Students with name length shorter than #{length}: "
-  students.each do |student|
-    if student[:name].split("").count < length
-      puts "#{student[:name]} (#{student[:cohort]} cohort)"
-    end
-  end
-end
-
-def print_footer
-  puts "Overall, we have #{@students.count} great students"
-  puts ""
-end
-
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  # get the first name
-  name = gets.chomp
-  # while the name is not empty, repeat this code
-  while !name.empty? do
-    # add the student hash to the array
-    @students << {name: name, cohort: get_cohort, height: get_height, hobby: get_hobby}
-    if @students.count <= 1
-      puts "Now we have #{@students.count} student"
-    else
-      puts "Now we have #{@students.count} students"
-    end
-    # get another name from the user
-    name = gets.chomp
-  end
-  # return the array of students
-  @students
-end
-
-def get_cohort
-  cohort = ""
-  while (cohort!="november")&&(cohort!="february")&&(cohort!="april") do
-    puts "Please enter your cohort (november, february or april): "
-    cohort = gets.chomp!.downcase
-  end
-  return cohort.to_sym
-end
-
-def get_hobby
-  hobby = ""
-  puts "Please enter hobby: "
-  return gets.chomp!
-end
-
-def get_height
-  hobby = ""
-  puts "Please enter height: "
-  return gets.chomp!
-end
-
-def print_by_cohort(students)
-  cohorts = ["november", "february", "april"]
-  cohorts.each do |cohort|
-    puts "#{cohort.capitalize} Cohort"
-    students.each do |student|
-      if student[:cohort].to_s == cohort
-        puts "#{student[:name].ljust(30)} #{student[:height].ljust(5)} #{student[:hobby].ljust(10)}"
-      end
-    end
-  end
-end
-
-interactive_menu
+# def get_cohort
+#   cohort = ""
+#   while (cohort!="november")&&(cohort!="february")&&(cohort!="april") do
+#     puts "Please enter your cohort (november, february or april): "
+#     cohort = gets.chomp!.downcase
+#   end
+#   return cohort.to_sym
+# end
+#
+# def get_hobby
+#   hobby = ""
+#   puts "Please enter hobby: "
+#   return gets.chomp!
+# end
+#
+# def get_height
+#   hobby = ""
+#   puts "Please enter height: "
+#   return gets.chomp!
+# end
+#
+# def print_by_cohort(students)
+#   cohorts = ["november", "february", "april"]
+#   cohorts.each do |cohort|
+#     puts "#{cohort.capitalize} Cohort"
+#     students.each do |student|
+#       if student[:cohort].to_s == cohort
+#         puts "#{student[:name].ljust(30)} #{student[:height].ljust(5)} #{student[:hobby].ljust(10)}"
+#       end
+#     end
+#   end
+# end
