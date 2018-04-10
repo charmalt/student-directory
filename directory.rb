f8e9f59053570bc5d4ae0a6b30aae6979b6bfed7
Charlene Chetcuti <cchetcu3@MGC000003514.warley.ford.com>
2018-04-10 15:55:03 +0100

    added save functionality

--- directory.rb
+++ directory.rb
@@ -10,13 +10,14 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
 def show_students
   print_header
-  print(@students)
-  print_footer(@students)
+  print_students_list
+  print_footer
 end
 
 def process(selection)
@@ -25,11 +26,25 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
       puts "I don't know what you mean, try again"
   end
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 # students = [
 #   {name: "Dr. Hannibal Lecter", cohort: :february, height: '5\'11', hobby: "eating"},
@@ -50,8 +65,8 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each_with_index do |student, index|
+def print_students_list
+  @students.each_with_index do |student, index|
     puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
@@ -88,8 +103,8 @@ def print_by_name_length(students, length)
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
   puts ""
 end
 
