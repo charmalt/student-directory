47cd7e1674faf21d8b1b16cd07dbd98bcdfec9b6
Charlene Chetcuti <cchetcu3@MGC000003514.warley.ford.com>
2018-04-11 12:23:37 +0100

    refactoring of student directory

--- directory.rb
+++ directory.rb
@@ -11,15 +11,9 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
+  puts "3. Save the list to file"
+  puts "4. Load the list from file"
   puts "9. Exit" # 9 because we'll be adding more items
-end
-
-def show_students
-  print_header
-  print_students_list
-  print_footer
 end
 
 def process(selection)
@@ -39,10 +33,32 @@ def process(selection)
   end
 end
 
+def input_students
+  name = request_student_name # request first student name
+  while !name.empty? do
+    add_student_to_array(name)
+    puts "There are currently #{@students.count} enrolled students."
+    name = request_student_name
+  end
+end
+
+def request_student_name
+  print "Enter the names of the student or hit return twice to return:"
+  STDIN.gets.chomp
+end
+
+def add_student_to_array(name)
+  @students << {name: name, cohort: :november}
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
 def save_students
-  # open the file for writing
   file = File.open(request_filename, "w")
-  # iterate over the array of students
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
@@ -78,21 +94,6 @@ end
 def print_footer
   puts "Overall, we have #{@students.count} great students"
   puts ""
-end
-
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  name = STDIN.gets.chomp
-  while !name.empty? do
-    add_student_to_array(name)
-    puts "Now we have #{@students.count} students"
-    name = STDIN.gets.chomp
-  end
-end
-
-def add_student_to_array(name)
-  @students << {name: name, cohort: :november}
 end
 
 def try_load_students
